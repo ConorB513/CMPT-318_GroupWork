36fc41b542e94bd901c19d0338f9ad4141a2e40b
cjb22 <cjb22@sfu.ca>
2025-01-29 15:51:18 -0800

    Print statements in part 1

--- Assignment_#1/318Assign1.R
+++ Assignment_#1/318Assign1.R
@@ -3,7 +3,7 @@ library(zoo)
 
 install.packages("dplyr")
 library(dplyr)
-dataset <- read.csv("./Group_Assignment_Dataset/Group_Assignment_Dataset.txt")
+dataset <- read.csv("./Group_Assignment_Dataset.txt")
 #convert to date format
 dataset$Date <- as.Date(dataset$Date, format="%d/%m/%Y")
 
@@ -24,39 +24,56 @@ colSums(is.na(one_week_data1))
 allGrp <- one_week_data1[,c("Global_reactive_power")];
 grpZScores <- (allGrp - mean(allGrp)) / sd(allGrp);
 numOutliersGrp <- length(grpZScores[abs(grpZScores) > 3]);
-print(paste0("number of outliers for Global Reactive Power = ", numOutliers));
-print(numOutliersGrp/length(grpZScores) * 100);
+print(paste0("number of outliers for Global Reactive Power = ", numOutliersGrp));
+print((numOutliersGrp/length(grpZScores)) * 100);
+print(numOutliersGrp);
+print(length(grpZScores));
+
+
 
 allGap <- one_week_data1[,c("Global_active_power")];
 gapZScores <- (allGap - mean(allGap)) / sd(allGap);
 numOutliersGrp <- length(gapZScores[abs(gapZScores) > 3]);
-print(numOutliersGap/length(gapZScores)) * 100;
+print((numOutliersGrp/length(gapZScores)) * 100);
+print(numOutliersGrp);
+print(length(gapZScores));
 
 
 allVolt <- one_week_data1[,c("Voltage")];
 voltZScores <- (allVolt - mean(allVolt)) / sd(allVolt);
-numOutliersVolt <- length(grpZScores[abs(grpZScores) > 3]);
-print(numOutliersVolt/length(voltZScores)) * 100;
+numOutliersVolt <- length(voltZScores[abs(voltZScores) > 3]);
+print((numOutliersVolt/length(voltZScores)) * 100);
+print(numOutliersVolt);
+print(length(voltZScores));
 
 allGI <- one_week_data1[,c("Global_intensity")];
 giZScores <- (allGI - mean(allGI)) / sd(allGI);
 numOutliersGI <- length(giZScores[abs(giZScores) > 3]);
-print(numOutliersGI/length(giZScores)) * 100;
+print((numOutliersGI/length(giZScores)) * 100);
+print(numOutliersGI);
+print(length(giZScores));
 
 allSM1 <- one_week_data1[,c("Sub_metering_1")];
 sm1ZScores <- (allSM1 - mean(allSM1)) / sd(allSM1);
 numOutliersSM1 <- length(sm1ZScores[abs(sm1ZScores) > 3]);
-print(numOutliersSM1/length(sm1ZScores)) * 100;
+print((numOutliersSM1/length(sm1ZScores)) * 100);
+print(numOutliersSM1);
+print(length(sm1ZScores));
 
 allSM2 <- one_week_data1[,c("Sub_metering_2")];
 sm2ZScores <- (allSM2 - mean(allSM2)) / sd(allSM2);
 numOutliersSM2 <- length(sm2ZScores[abs(sm2ZScores) > 3]);
-print(numOutliersSM2/length(sm2ZScores)) * 100;
+print((numOutliersSM2/length(sm2ZScores)) * 100);
+print(numOutliersSM2);
+print(length(sm2ZScores));
 
 allSM3 <- one_week_data1[,c("Sub_metering_3")];
 sm3ZScores <- (allSM3 - mean(allSM3)) / sd(allSM3);
 numOutliersSM3 <- length(sm3ZScores[abs(sm3ZScores) > 3]);
-print(numOutliersSM3/length(sm3ZScores)) * 100;
+print((numOutliersSM3/length(sm3ZScores)) * 100);
+print(numOutliersSM3);
+print(length(sm3ZScores))
+
 
 
 
