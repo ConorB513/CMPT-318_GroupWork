8d29b1cf48a037d108d21a3ce080536e0ec737be
seanAColoma <138170020+seanAColoma@users.noreply.github.com>
2025-03-13 14:25:29 -0700

    Add files via upload

--- termProject.R
+++ termProject.R
@@ -4,29 +4,43 @@ library(stats);
 install.packages("zoo");
 library(zoo);
 
-dataset <- read.csv("./TermProjectData.txt", header = TRUE);
+dataset <- read.csv("./TermProjectData.txt");
 
 #1 - scale features - standardization with na values linearly interpolated
-gap <- na.approx(dataset$Global_active_power);
+
+# - rule = 2 extends the first non na value to the beginning/end if it is na
+gap <- na.spline(dataset$Global_active_power);
 dataset$Global_active_power <- (gap-mean(gap))/sd(gap);
 
-grp <- na.approx(dataset$Global_reactive_power);
+grp <- na.spline(dataset$Global_reactive_power);
 dataset$Global_reactive_power <- (grp - mean(grp))/sd(grp);
 
-volt <- na.approx(dataset$Voltage);
+volt <- na.spline(dataset$Voltage);
 dataset$Voltage <- (volt - mean(volt))/sd(volt);
 
-gi <- na.approx(dataset$Global_intensity);
+gi <- na.spline(dataset$Global_intensity);
 dataset$Global_intensity <- (gi - mean(gi))/sd(gi);
 
-sm1 <- na.approx(dataset$Sub_metering_1);
+sm1 <- na.spline(dataset$Sub_metering_1);
 dataset$Sub_metering_1 <- (sm1 - mean(sm1))/sd(sm1);
 
-sm2 <- na.approx(dataset$Sub_metering_2);
+sm2 <- na.spline(dataset$Sub_metering_2);
 dataset$Sub_metering_2 <- (sm2 - mean(sm2))/sd(sm2);
 
-sm3 <- na.approx(dataset$Sub_metering_3);
+sm3 <- na.spline(dataset$Sub_metering_3);
 dataset$Sub_metering_3 <- (sm3 - mean(sm3))/sd(sm3);
 
+
+#2 - principal component analysis
+#extract only the numerical columns
+numerical <- dataset[c("Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")];
+
+#conduct PCA
+pcs <- prcomp(numerical);
+summary(pcs);
+
+#plot PCA
+
+#xx% of the variance is accounted for with the first x PCs, use those going
 
 
